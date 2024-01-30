567787e6afa8d4cf5fb3294b135e62c8e4b600db
Quarshie <asi37375@hotmail.com>
2024-01-30 18:34:23 +0000

    formating function to display hour

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ $(document).ready(function () {
         var currentDate = dayjs().format("dddd, MMMM D");
         $("#currentDay").text("Today is " + currentDate);
     }
+
+      // Function to format the hour for display
+   function formatHour(hour) {
+    return dayjs().hour(hour).format("h A");
+}
+
