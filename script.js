6d0e092c82bf6fc7f100490a0e86a63934c2394c
Quarshie <asi37375@hotmail.com>
2024-01-30 18:29:52 +0000

    css style added for present, past annd future

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+// script.js
+
 $(document).ready(function () {
-    // Function to display the current day at the top of the calendar
+    // Function to display the current day
     function displayCurrentDay() {
         var currentDate = dayjs().format("dddd, MMMM D");
         $("#currentDay").text("Today is " + currentDate);
     }
-
-   
